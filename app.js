c62c6c043f09b20020d6dbe980328c06bc6df054
Volodymyr Lytvynenko <116517609+volodyaaaaaa@users.noreply.github.com>
2023-12-13 23:45:19 +0000

    test commit

---
+++ app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+class BlankScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: 'white' }} />
+    );
+  }
+}
+
+export default BlankScreen;
